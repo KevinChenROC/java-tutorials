17c91440f0bdadbd28fa65a5070bee5c89aac7ed
Kuan-Wen Chen <kev888kev@gmail.com>
2021-07-18 23:02:00 +0800

    Java primitive types

---
+++ variables/PrimitivesDataTypes.java
@@ -0,0 +1,11 @@
+public class PrimitivesDataTypes {
+    public static void main(String[] args) {
+        int a = 10, b = 10;  // Example of initialization
+        byte B = 22;         // initializes a byte type variable B.
+        double pi = 3.14159; // declares and assigns a value of PI.
+        char c = 'a';        // the char variable a iis initialized with value 'a'
+        float float_num = 4.4f; //float 
+        boolean t = false; //boolean
+        
+    }
+}
