fe850c3e1efe85018866d58e8ade68e9d4b0303b
Kuan-Wen Chen <kev888kev@gmail.com>
2021-07-19 00:52:35 +0800

    New wrapper class usage

--- OOP/AbstractClass.java
+++
@@ -1,8 +0,0 @@
-abstract class AbstractClass {
-   private double price;
-   private String model;
-   private String year;
-   public abstract void goFast();   // an abstract method
-   public abstract void changeColor();
-   public void sayHello(){}; // this produces an error!
-}
--- variables and data types/WrapperClass.java
+++ variables and data types/WrapperClass.java
@@ -3,6 +3,16 @@ public class WrapperClass {
    public static void main(String args[]) {
       Integer x = 5; // boxes int to an Integer object
       x =  x + 10;   // unboxes the Integer to a int
-      System.out.println(x);
+
+      /*
+      ==========calling wrapper class methods===================
+      */
+      System.out.println(x.toString());
+      System.out.println(x.floatValue());
+      System.out.println(x.doubleValue());
+      System.out.println(x.equals(1));
+
+      
+     
    }
 }
